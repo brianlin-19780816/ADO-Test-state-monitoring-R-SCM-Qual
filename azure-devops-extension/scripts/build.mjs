0cd7b5178cbd0e7082b485dad33a8a100a576d2f
brianlin-19780816 <brian.0816@gmail.com>
2026-09-08 11:08:48 +0800

    Update references to C4142 R-SCM userscript

--- azure-devops-extension/scripts/build.mjs
+++ azure-devops-extension/scripts/build.mjs
@@ -34,5 +34,5 @@ await Promise.all([
   copyFile(join(root, "src", "dashboard.html"), join(dist, "dashboard.html")),
   copyFile(join(root, "src", "widget.html"), join(dist, "widget.html")),
   copyFile(join(root, "src", "widget.css"), join(dist, "widget.css")),
-  copyFile(join(root, "..", "C4143-CRD4.1-Dashboard.user.js"), join(dist, "C4143-CRD4.1-Dashboard.user.js"))
+  copyFile(join(root, "..", "C4142-R-SCM-Dashboard.user.js"), join(dist, "C4142-R-SCM-Dashboard.user.js"))
 ]);
