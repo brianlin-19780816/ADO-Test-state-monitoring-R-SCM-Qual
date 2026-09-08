5791bf395bf8293a002fbdbeee913c93d24da6d5
brianlin-19780816 <brian.0816@gmail.com>
2026-09-08 10:57:20 +0800

    Point dashboard to R-SCM Qual Test Plan

--- C4143-CRD4.1-Dashboard.user.js
+++ C4143-CRD4.1-Dashboard.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         C4143 CRDv4.1 Qual Test Status Dashboard
 // @namespace    local.ado.dvscale.dashboard
-// @version      1.11.4
+// @version      1.11.5
 // @description  Adds a multi-project Query selector, real Test Results, XLSX exports, query-scoped snapshots, and Extension support.
 // @homepageURL  https://github.com/brianlin-19780816/ADO-Test-state-monitoring-C4143-CRDv4.1-Qual
 // @supportURL   https://github.com/brianlin-19780816/ADO-Test-state-monitoring-C4143-CRDv4.1-Qual/issues
@@ -57,17 +57,17 @@
   var isDashboardEntry = !!extensionContext || location.hash.toLowerCase().indexOf("crdv41qual") >= 0;
   if (!isDashboardEntry) return;
   var D = {};
-  D.CFG = {"org":"https://azurecsi.visualstudio.com","orgName":"azurecsi","project":"Dev","sourceType":"testPlan","planId":2783433,"suiteId":3942624,"queryId":"","queryUrl":"https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=2783433&suiteId=3942624","testResultDays":28};
+  D.CFG = {"org":"https://azurecsi.visualstudio.com","orgName":"azurecsi","project":"Dev","sourceType":"testPlan","planId":3995643,"suiteId":3995646,"queryId":"","queryUrl":"https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=3995643&suiteId=3995646","testResultDays":28};
   if (extensionContext) {
     D.CFG.org = String(extensionContext.org || D.CFG.org).replace(/\/+$/, '');
     D.CFG.orgName = extensionContext.orgName || D.CFG.orgName;
     D.CFG.project = extensionContext.project || D.CFG.project;
     D.CFG.queryUrl = D.CFG.orgName === 'azurecsi' && D.CFG.project === 'Dev'
-      ? 'https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=2783433&suiteId=3942624'
+      ? 'https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=3995643&suiteId=3995646'
       : D.CFG.org + '/' + encodeURIComponent(D.CFG.project) + '/_queries/query/' + D.CFG.queryId + '/';
   }
   D.DEFAULT_QUERIES = [
-    { name: 'C4143_CRDv4.1 Qual', org: 'https://azurecsi.visualstudio.com', orgName: 'azurecsi', project: 'Dev', sourceType: 'testPlan', planId: 2783433, suiteId: 3942624, queryId: '', queryUrl: 'https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=2783433&suiteId=3942624', builtin: true },
+    { name: 'C4143_CRDv4.1 Qual', org: 'https://azurecsi.visualstudio.com', orgName: 'azurecsi', project: 'Dev', sourceType: 'testPlan', planId: 3995643, suiteId: 3995646, queryId: '', queryUrl: 'https://azurecsi.visualstudio.com/Dev/_testPlans/charts?planId=3995643&suiteId=3995646', builtin: true },
     { name: '[EchoFalls][C4142][PSE] EVT - Scale Testing', org: 'https://azurecsi.visualstudio.com', orgName: 'azurecsi', project: 'Dev', queryId: '6e06c765-2ff5-43c4-80c6-e78438eea6d9', queryUrl: 'https://azurecsi.visualstudio.com/Dev/_queries/query/6e06c765-2ff5-43c4-80c6-e78438eea6d9/', builtin: true }
   ];
   D.STATE_COLORS = {"Not Started":"#94a3b8","New":"#60a5fa","Proposed":"#f5b544","Design":"#a78bfa","In Progress":"#818cf8","Active":"#818cf8","Ready":"#38bdf8","Committed":"#22d3ee","Passed":"#34d399","Closed":"#2dd4bf","Done":"#2dd4bf","Completed":"#2dd4bf","Failed":"#f87171","Blocked":"#fb7185","Removed":"#9ca3af","Resolved":"#22d3ee","Paused":"#fbbf24"};
